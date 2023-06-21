ae61d518b3a6b06d416113ed21fa134b0e35b111
kocak <streax14@gmail.com>
2023-06-22 00:50:46 +0200

    blink

---
+++ section2/blinkTb.cpp
@@ -0,0 +1,25 @@
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vblink.h"
+
+#define MAX_SIM_TIME 20
+
+int main(int argc, char** argv, char**env){
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+    vluint64_t simTime = 0;
+    Vblink* top = new Vblink;
+
+    
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    top->trace(m_trace, 99);
+    m_trace->open("waveform.vcd");
+    while(simTime < MAX_SIM_TIME){
+        top->clk ^= 1;
+        top->eval();
+        m_trace->dump(simTime);
+        simTime++;
+    }
+    delete top;
+    exit(EXIT_SUCCESS);
+}
